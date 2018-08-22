3124edbf8eafa0c5d70280e439982a8921492cd9
kalug89 <kaluzny_david@hotmail.com>
2018-08-22 22:28:20 +0200

    Add Toast to click image view

--- app/src/main/java/pl/dkaluzny/animalsbio/MainActivity.kt
+++ app/src/main/java/pl/dkaluzny/animalsbio/MainActivity.kt
@@ -1,12 +1,33 @@
 package pl.dkaluzny.animalsbio
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            chetachId.id -> {
+                Toast.makeText(this, "Cheetah", Toast.LENGTH_LONG).show()
+            }
+            lionId.id -> {
+                Toast.makeText(this, "Lion", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var chetah = chetachId
+        var lion = lionId
+
+        //register out view to recive onCLickListener events
+
+        chetah.setOnClickListener(this)
+        lion.setOnClickListener(this)
     }
 }
